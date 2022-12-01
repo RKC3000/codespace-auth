07fcbc29d5fca66bf17ba757c5960a2a21a60905
Ramakrishna Chhipa <54206397+RKC3000@users.noreply.github.com>
2022-12-01 04:54:20 +0000

    In progress

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ app.get("/", (req, res) => {
 app.post("/register", (req, res) => {
   try {
     // get all data from body
+    const {firstname, lastname, email, password} = req.body
     // all the data should exists
+    if (!(firstname && lastname && email && password)) {
+      res.status(400).send('All fields are compulsory')
+    }
     // check if user already existes
     // encrypt the password
     // save the user in DB
     // generate a token for user and send it
-    
   } catch (error) {
     console.log(error);
   }
