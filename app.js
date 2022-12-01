6395cc43a080907432c2162088faf400a981a0af
Ramakrishna Chhipa <54206397+RKC3000@users.noreply.github.com>
2022-12-01 04:33:56 +0000

    Making the post method

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ require("./database/database").connect();
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Server is working</h1>");
+});
+
+app.post("/register", (req, res) => {
+  try {
+    // get all data from body
+    // all the data should exists
+    // check if user already existes
+    // encrypt the password
+    // save the user in DB
+    // generate a token for user and send it
+    
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = app;
